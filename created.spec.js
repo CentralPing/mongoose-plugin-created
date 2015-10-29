d416168b35e80f8173933a91613b1eb46f087038
JasonCust <jason.cust@gmail.com>
2015-10-29 00:16:59 -0400

    test: allow mongo host and port to be specified via env variables

--- created.spec.js
+++ created.spec.js
@@ -4,6 +4,12 @@
 var mongoose = require('mongoose');
 var faker = require('faker');
 var created = require('./created');
+
+var connectionString = 'mongodb://' +
+  (process.env.MONGO_HOST || 'localhost') +
+  (process.env.MONGO_PORT ? ':' + process.env.MONGO_PORT : '') +
+  '/unit_test';
+
 var Schema = mongoose.Schema;
 var connection;
 
@@ -21,7 +27,7 @@ var blogData = {
 beforeAll(function (done) {
   mongoose.set('debug', process.env.DEBUG || false);
 
-  connection = mongoose.createConnection('mongodb://localhost/unit_test');
+  connection = mongoose.createConnection(connectionString);
   connection.once('connected', function () {
     done();
   });
